eaf0af81c6a06ce3a188ff5ab271320c632a0a26
Yavuzhan Terzi <yavuzhanterzi@gmail.com>
2022-11-05 07:39:15 +0300

    JavaDers3

--- src/oopWithNLayeredApp/Main.java
+++ src/oopWithNLayeredApp/Main.java
@@ -1,7 +1,15 @@
 package oopWithNLayeredApp;
 
+import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.entities.Product;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Product product1 = new Product(1,"Iphone 11",15000);
+
+        ProductManager productManager = new ProductManager(new HibernateProductDao());
+        productManager.add(product1);
 
 
     }
--- src/oopWithNLayeredApp/business/ProductManager.java
+++ src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,9 +1,28 @@
 package oopWithNLayeredApp.business;
 
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class ProductManager {
-    public void add(Product product){
+    private ProductDao productDao;
+    private Logger[] loggers;
+
+    public ProductManager(ProductDao productDao) {
+        this.productDao = productDao;
+        this.loggers = loggers;
+    }
+
+    public void add(Product product) throws Exception {
+        //iş kuralları
+        if (product.getUnitPrice()<10){
+            throw new Exception("Ürün fiyatı 10 dan küçük olamaz");
+        }
+        //ProductDao productDao = new HibernateProductDao();
+        productDao.add(product);
+
+
+
 
     }
 }
